e425ba80351f3a6d6536a6b1a9e834ae45ae9e90
Anurag Pramanik <p.anurag@outlook.com>
2022-05-05 21:36:23 +0530

    add jwt authentication and 'me' api

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,7 +3,6 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const User = require('../models/userModel')
-const { use } = require('../routes/userRoutes')
 
 // @desc Register a new user
 // @route /api/users
@@ -72,6 +71,18 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc Get current user
+// @route /api/users/me
+// @access Private
+const getMe = asyncHandler(async (req, res) => {
+  const user = {
+    id: req.user._id,
+    email: req.user.email,
+    name: req.user.name,
+  }
+  res.status(200).json(user)
+})
+
 // Generate token
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -82,4 +93,5 @@ const generateToken = (id) => {
 module.exports = {
   registerUser,
   loginUser,
+  getMe,
 }
