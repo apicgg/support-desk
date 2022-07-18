2554676d18b72d84fa825ae503c8d83079ee8110
Anurag Pramanik <p.anurag@outlook.com>
2022-07-18 21:09:26 +0530

    Add .env_example and some comments

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,6 +10,7 @@ const User = require('../models/userModel')
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body
 
+  // Validation
   if (!name || !email || !password) {
     res.status(400)
     throw new Error('Please include all the fields')
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv').config()
 const colors = require('colors')
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 // Connect to DB
 connectDB()
@@ -13,6 +13,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// API Route
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the Support Desk API' })
 })
